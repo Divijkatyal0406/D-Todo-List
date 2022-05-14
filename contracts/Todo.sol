57c201f3f17dce5269131ee7cd0d294800603fe6
Divij Katyal <divijkatyal@gmail.com>
2022-05-14 18:49:43 +0530

    contract-and-web3

--- contracts/Todo.sol
+++ contracts/Todo.sol
@@ -2,18 +2,29 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract TodoList{
 
-    uint taskCount=0;
+    uint public taskCount=0;
     struct Task{
         uint id;
         string content;
         uint time;
         uint priority;
+        bool completed;
     }
-    mapping(uint=>Task) tasks;
 
-    function createTask(string memory content,uint time) public{
-        tasks[taskCount]=Task(taskCount,content,block.timestamp,block.timestamp-time);
+    event TaskEvent(string _content,uint time);
+
+
+    mapping(uint=>Task) public tasks;
+
+    constructor() public{
+        createTask("Test todo", block.timestamp);
+    }
+
+    function createTask(string memory _content,uint time) public{
         taskCount++;
+        uint priority=block.timestamp-time;
+        tasks[taskCount]=Task(taskCount,_content,block.timestamp,priority,false);
+        emit TaskEvent(_content,tasks[taskCount].time);
     }
 
     function deleteTask(uint id) public{
